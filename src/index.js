eabbbe874053ec60f10bbc81fa06e6299edfe088
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-06-08 13:36:17 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,18 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+  
+  document.querySelector(".btn-primary").addEventListener("click", function(event) {
+    fetch("https://randomuser.me/api/")
+  .then(response => response.json())
+  .then(json => infoCard(json["results"][0]))
+  })
+
+  function infoCard(user) {
+    // Fill in name
+    document.getElementById("fullname").innerText = user.name.title + " " + user.name.first + " " + user.name.last 
+
+    // Repeat the same scheme for all other fields.....
+  }
+  
 });
